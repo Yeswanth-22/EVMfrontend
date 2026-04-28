b5ef93db0c38af4ae23dd120e89113327b8da8cd
Yeswanth-22 <2400031534@kluniversity.in>
2026-04-28 11:21:12 +0530

    final deploy

--- src/api.js
+++ src/api.js
@@ -1,3 +1,5 @@
-const API_BASE_URL = "https://evmbackend-n3qk.onrender.com";
+export const API_URL = "https://evmbackend-n3qk.onrender.com";
+
+const API_BASE_URL = API_URL;
 
 export default API_BASE_URL;
